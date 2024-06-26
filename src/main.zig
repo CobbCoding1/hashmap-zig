5881700bdbae3dee77b1bd698192cc51554ffaba
CobbCoding <rircobb@gmail.com>
2024-06-26 16:55:36 -0400

    wip

--- src/main.zig
+++ src/main.zig
@@ -13,46 +13,58 @@ const Elem = struct {
     key: [*:0]const u8,
     is_used: bool,
 };
-
-const capacity: usize = 32;
-var val: [capacity]Elem = undefined;
-var map_s: usize = 0;
-
-pub fn hash(n: [*:0]const u8) usize {
-    var result: usize = 0;
-    for (0..std.mem.len(n)) |i| {
-        result += n[i] + 10 * 3;
+    
+const Map = struct {
+    capacity: usize,    
+    pub var val: std.ArrayList(Elem) = undefined;
+    pub var map_s: usize = 0;
+    
+    pub fn hash(n: [*:0]const u8) usize {
+        var result: usize = 0;
+        for (0..std.mem.len(n)) |i| {
+            result += n[i] + 10 * 3;
+        }
+        return result;
     }
-    return result;
-}
-
-pub fn insert(key: [*:0]const u8, value: u32) !void {
-    if (map_s == capacity) return InsertError.MapFull;
-    var index: usize = hash(key) % capacity;
-    while (val[index].is_used == true) {
-        if (index > capacity) index = 0;
-        index += 1;
+    
+    pub fn insert(self: Map, key: [*:0]const u8, value: u32) !void {
+        if (map_s >= self.capacity) return InsertError.MapFull;
+        var index: usize = hash(key) % self.capacity;
+        while (val.items[index].is_used) {
+            if (index > self.capacity) index = 0;
+            index += 1;
+        }
+        try val.append(undefined);
+        val.items[index] = Elem{
+            .value = value,
+            .key = key,
+            .is_used = true,
+        };
+        map_s += 1;
     }
-    val[index] = Elem{
-        .value = value,
-        .key = key,
-        .is_used = true,
-    };
-    map_s += 1;
-}
-
-pub fn get(key: [*:0]const u8) !u32 {
-    for (0..capacity) |i| {
-        if (val[i].key == key) return val[i].value;
+    
+    pub fn get(self: Map, key: [*:0]const u8) !u32 {
+        for (0..self.capacity) |i| {
+            if (val.items[i].key == key) return val.items[i].value;
+        }
+        return GetError.KeyNotFound;
     }
-    return GetError.KeyNotFound;
-}
+        
+    pub fn init() !Map {
+        const allocator = std.heap.page_allocator;        
+        map_s = 0;
+        val = std.ArrayList(Elem).init(allocator);
+        val.items = try val.addManyAsArray(32);
+        return Map{.capacity = 32};
+    }
+};
 
 pub fn main() !void {
-    try insert("test", 32);
-    try insert("test1", 16);
-    try insert("test2", 8);
-    try insert("test3", 2);
-    const result: u32 = try get("test3");
+    var map: Map = try Map.init();
+    try map.insert("test", 32);
+    try map.insert("test1", 16);
+    try map.insert("test2", 8);
+    try map.insert("test3", 2);
+    const result: u32 = try map.get("test2");
     std.debug.print("Hello, world! {d}\n", .{result});
 }
